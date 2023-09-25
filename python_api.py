c5ad30f768a6cde130ee6bfe7394378fa1486eec
gipsyDANGER2810 <swapnilgalhate1036@gmail.com>
2023-09-25 21:41:20 +0100

    commit

--- python_api.py
+++ python_api.py
@@ -12,49 +12,64 @@ CORS(app)
 @app.route("/recommend", methods=['GET'])
 def recommend():
     userid = request.args.get('userid')
-    print(userid)
-    print(request.args)
+
     # Ensure the Python script is in your system path
     if userid:
         
         # Import the function
         from collaborative import popular_products
-        from content_based_rs2 import recommend_products_with_profiles , userIdEncoder
+        from content_based_rs2 import recommend_products_with_profiles 
+        from hybrid import get_recommendations, hybrid_recommendation_v2 , userIdEncoder
+        
         # Get recommendations
         recommendations = popular_products()
         userIdEncoded = userIdEncoder(userid)
-        print(userIdEncoded)
+        # print(userIdEncoded)
+        # recommendations2 = recommend_products_with_profiles(userIdEncoded)
         recommendations2 = recommend_products_with_profiles(userIdEncoded)
-        recommendations2 = recommend_products_with_profiles(4773)
-        # recommendations2 = pd.DataFrame(recommendations2)
+        recommendations2 = pd.DataFrame(recommendations2)
 
         # Convert DataFrame to a dictionary
         recommendations_dict = recommendations.to_dict(orient='records')
         recommendations_dict_2 = recommendations2.to_dict(orient='records')
         # Return recommendations as a JSON
-        return jsonify({'first_set': recommendations_dict, 'second_set': recommendations_dict_2})
+        return jsonify({'Popular_products': recommendations_dict, 'content_based_products': recommendations_dict_2})
     else:
         from collaborative import popular_products
         from content_based_rs2 import recommend_products_with_profiles , userIdEncoder
         
         recommendations = popular_products()
         recommendations_dict = recommendations.to_dict(orient='records')
-        recommendations2 = recommend_products_with_profiles(4773)
+        recommendations2 = popular_products()
         recommendations2 = pd.DataFrame(recommendations2)
         recommendations_dict_2 = recommendations2.to_dict(orient='records')
-        return jsonify({'first_set': recommendations_dict, 'second_set': recommendations_dict_2})
-        # sys.path.insert(0, 'C:/Users/swapn/Desktop/final rs/')
-        # print(product_name)
-        # from collaborative import get_recommendations
-        # from content_based_rs2 import recommend_products_with_profiles
-        # recommended_products_cf = get_recommendations(product_name)
-        # recommended_products_cb = recommend_products_with_profiles(4773)
-        # if recommended_products_cf==[]:
-        #     print("hi")
-        # # recommendations = recommended_products
-        # response = {'collaborative':recommended_products_cf, 'contentBased':recommended_products_cb}
-        # return jsonify(response)
+        return jsonify({'Popular_products': recommendations_dict, 'content_based_products': recommendations_dict_2})
 
+
+
+@app.route("/cart" , methods=['GET'])
+def get_collab_recommendations():
+    userid = request.args.get('userid')
+
+
+    from hybrid import get_recommendations , userIdEncoder
+    
+    userIdEncoded = userIdEncoder(userid)
+    recommendation = get_recommendations(userIdEncoded)
+    recommendations_dict = recommendation.to_dict(orient='records')
+    return jsonify({'Collab_products': recommendations_dict})
+
+@app.route("/hybrid" , methods=['GET'])
+def get_hybrid_recommendations():
+    userid = request.args.get('userid')
+
+
+    from hybrid import hybrid_recommendation_v2 , userIdEncoder
+    
+    userIdEncoded = userIdEncoder(userid)
+    recommendation = hybrid_recommendation_v2(userIdEncoded)
+    recommendations_dict = recommendation.to_dict(orient='records')
+    return jsonify({'hybrid_products': recommendations_dict})
 
 @app.route("/categories")
 def get_categories():
